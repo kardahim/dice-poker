a42c19b6cb91d4773ccd93dc49d1b1a76a266352
Paweł Ciszewski <kardahim@gmail.com>
2023-12-11 23:25:20 -0800

    Initial Commit

---
+++ dice poker/dice_pokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_pokerApp.swift
+//  dice poker
+//
+//  Created by Kardahim on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct dice_pokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
